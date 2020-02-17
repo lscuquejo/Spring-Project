3cccc827fee629dea620b039a69359a6828830c9
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2020-02-17 14:45:19 +0000

    created first test using mokito framwork

---
+++ src/test/java/com/training/app/ws/service/implementation/AccountServiceImplementationTest.java
@@ -0,0 +1,49 @@
+package com.training.app.ws.service.implementation;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+
+import com.training.app.ws.io.entity.AccountEntity;
+import com.training.app.ws.repository.AccountRepository;
+import com.training.app.ws.shared.dto.AccountDto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.util.Assert;
+
+class AccountServiceImplementationTest {
+    
+    @InjectMocks
+    AccountServiceImplementation accountService;
+
+    @Mock
+    AccountRepository accountRepository;
+
+    @BeforeEach
+    void setUp()throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    final void testGetAccount() {
+
+        AccountEntity accountEntity = new AccountEntity();
+
+        accountEntity.setId(1L);
+        accountEntity.setBalance(BigDecimal.valueOf(2000));
+        accountEntity.setUid("123123");
+        accountEntity.setName("mocked");
+        accountEntity.setTreasury(false);
+
+        when(accountRepository.findByUid(anyString())).thenReturn(accountEntity);
+        
+        AccountDto accountDto = accountService.getAccountByUId("123123");
+
+        Assert.notNull(accountDto, "accountDTO is null");
+    }
+}
