e35869f8e1326a1ed221e66bbdef82ba6fbd323f
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2020-02-17 12:47:44 +0000

    created new test case and tested it

---
+++ src/test/java/com/training/app/ws/implementation/AccountImplmentationTest.java
@@ -0,0 +1,17 @@
+package com.training.app.ws.implementation;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AccountImplementationTest {
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @Test
+    final void testGetAccount() {
+        fail("Not yet implemented");
+    }
+}
